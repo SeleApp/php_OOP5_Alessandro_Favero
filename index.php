effe4f4f639f88a4854495f4f10b38649d909a28
SeleApp <seleapp.info@gmail.com>
2026-02-23 11:49:46 +0100

    Index

--- index.php
+++ index.php
@@ -2,15 +2,43 @@
 
 require_once __DIR__ . '/class.php';
 
-$categories = [
-	new Attualita(),
-	new Sport(),
-	new Gossip(),
-	new Storia(),
+class Post
+{
+	private $titolo;
+	private $categoria;
+	private $tag;
+
+	public function __construct($titolo, Category $categoria, array $tag)
+	{
+		$this->titolo = $titolo;
+		$this->categoria = $categoria;
+		$this->tag = $tag;
+	}
+
+	private function getCategoryLabel()
+	{
+		ob_start();
+		$this->categoria->getMyCategory();
+		return trim(ob_get_clean());
+	}
+
+	public function showCompleteArticle()
+	{
+		echo '<h3>' . $this->titolo . '</h3>';
+		echo '<p><strong>Categoria:</strong> ' . $this->getCategoryLabel() . '</p>';
+		echo '<p><strong>Tag:</strong> ' . implode(', ', $this->tag) . '</p>';
+		echo '<hr>';
+	}
+}
+
+$posts = [
+	new Post('Le notizie del giorno', new Attualita(), ['news', 'italia']),
+	new Post('Risultati della domenica', new Sport(), ['calcio', 'serie-a']),
+	new Post('Retroscena dal mondo VIP', new Gossip(), ['celebrità', 'tv']),
+	new Post('La Roma antica in breve', new Storia(), ['impero-romano', 'cultura']),
 ];
 
-foreach ($categories as $category) {
-	$category->getMyCategory();
-	echo '<br>';
+foreach ($posts as $post) {
+	$post->showCompleteArticle();
 }
 
